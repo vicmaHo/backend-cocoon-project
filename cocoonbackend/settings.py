3529689653c7d9c094ca2c4fed8ac95e06d62828
Victor Hernandez <victormanu968@gmail.com>
2024-09-15 13:05:49 -0500

    fix sintaxis

--- cocoonbackend/settings.py
+++ cocoonbackend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-$(eod^0d@cbmi-nugjs&pf%irg55(&pc=%z_cc@m#04kwra@i$'))
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-$(eod^0d@cbmi-nugjs&pf%irg55(&pc=%z_cc@m#04kwra@i$')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ # en desarrollo es true, en produccion es false
 
