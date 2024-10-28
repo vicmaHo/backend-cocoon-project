ee130d6bc339479fa06d265bfb92678abd2a2d50
Victor Hernandez <victormanu968@gmail.com>
2024-10-28 18:03:46 -0500

    fix password serializer

--- usuarios/serializers.py
+++ usuarios/serializers.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields  = ['id', 'username', 'email', 'last_name', 'first_name']
+        fields  = ['id', 'username', 'email', 'last_name', 'first_name', 'password']
 
 
 class ArrendatarioSerializer(serializers.ModelSerializer):
